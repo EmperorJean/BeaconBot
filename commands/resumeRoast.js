2ffa319507aa9c774359a0fc8ae9fb71e5b267ae
Jean Gerard <jeangg@Jeans-MacBook-Air.local>
2024-04-16 22:29:35 -0400

    Fix pdf parser to parse correctly

--- commands/resumeRoast.js
+++ commands/resumeRoast.js
@@ -15,12 +15,23 @@ module.exports = {
             option.setName('resume')
                 .setRequired(true)
                 .setDescription('Your resume (PDF)')),
+    // .addStringOption(option =>
+    //     option.setName('level')
+    //         .setRequired(true)
+    //         .setDescription('The roast level')
+    //         .addChoices(
+    //             { name: 'MILD', value: '1' },
+    //             { name: 'MEDIUM', value: '2' },
+    //             { name: 'HOT', value: '3' },
+    //             { name: 'EXTRA HOT', value: '4' },
+    //             { name: 'MAX SPICE', value: '5' },
+    //         )),
     async execute(interaction) {
         try {
             const attachment = interaction.options.getAttachment('resume');
             const isEphemeral = interaction.channelId !== BOT_CHANNEL && interaction.channelId !== BOT_CHANNEL2;
-            let prompt = "I want you to roast this resume, don't hold back and be very verbose! ";
-
+            // let prompt = `I want you to roast this resume at level ${interaction.options.getString('level')} with level 1 being just feedback with no roast at all and 5 being a roast where you don't hold back at all, be very verbose! `;
+            let prompt = `I want you to roast this resume, be very verbose!`;
             // Let user know that the bot is processing the request
             await interaction.reply({ content: 'Cooking up your roast 😈 ...', ephemeral: isEphemeral });
 
@@ -32,7 +43,7 @@ module.exports = {
                         return interaction.editReply({ content: 'Could not get the contents of your resume, please make sure you are uploading a pdf file', ephemeral: isEphemeral });
                     }
                 });
-
+                
             const responseText = await generateContent(prompt);
             // Create an embed with the response
             const embed = new EmbedBuilder()
@@ -54,7 +65,21 @@ async function downloadAndExtractTextFromPDF(pdfUrl) {
         const dataBuffer = Buffer.from(response.data);
 
         // Parse the PDF
-        const data = await pdf(dataBuffer);
+        const options = {
+            max: 0,
+            pagerender: pageData => {
+                let render_options = {
+                    normalizeWhitespace: true,
+                };
+                return pageData.getTextContent(render_options)
+                    .then(textContent => {
+                        return textContent.items.map(item => item.str).join(' ');
+                    });
+            }
+        };
+
+        const data = await pdf(dataBuffer, options);
+
         return data.text;
     } catch (error) {
         console.error("Error fetching or parsing PDF:", error);
